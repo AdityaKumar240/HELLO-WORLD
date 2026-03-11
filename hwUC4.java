dcc437108715d153bdfc53f39eabd913fea987aa
Aditya <adityakumar6875@gmail.com>
2026-03-11 14:08:46 +0530

    initial commmit

---
+++ hwUC4.java
@@ -0,0 +1,28 @@
+public class hwUC4 {
+
+    public static void main(String[] args) {
+
+        String names;
+
+        if (args.length > 0) {
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                builder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    builder.append(", ");
+                }
+            }
+
+            names = builder.toString();
+
+        } else {
+            names = "World";
+        }
+
+        System.out.println("Hello, " + names + "!");
+    }
+
+}
